5c7efd672faee3821a849989d14dd961da10189e
ChuckGarciaN <chuckgarcian@gmail.com>
2024-02-04 14:09:42 -0600

    add updates to multiplier

--- multiplier/multiplier.sv
+++ multiplier/multiplier.sv
@@ -1,96 +1,163 @@
-module andlg # (parameter int D_SIZE) (
-  input wire [D_SIZE-1 : 0] a,
-  input wire [D_SIZE-1 : 0] b,
-  output logic [D_SIZE-1 : 0] res
-);
-
-  always_comb begin
-    res <= a && b;
-  end 
-endmodule
-
-module ripple_carry #(parameter int D_SIZE) (
-  input wire [D_SIZE-1 : 0] a,
-  input wire [D_SIZE-1 : 0] b,
-  output logic [D_SIZE-1 : 0] sum
-);
-  always_ff @(posedge) begin
-    sum <= a + b;
-  end 
-endmodule
-
-module multiplier #(parameter int D_SIZE) (
-  input wire[D_SIZE-1: 0] A,
-  input wire[D_SIZE-1: 0] B,
-  input wire clk_in;
-  output logic [D_SIZE-1:0] P
-
-);
-  
-  wire[D_SIZE-1:0] w1;
-  
-  // Registers to place operands and result in temprarly 
-  logic [D_SIZE-1:0] tempP = 0;
-  logic [D_SIZE-1:0] tempA = 0;  
-  logic [D_SIZE-1:0] tempB = 0;
-  
-  // Count number of multiplications made
-
-
-  // andlg #(.D_SIZE(D_SIZE)) a0 (
-  //   .a(w1), 
-  //   .b({D_SIZE{tempA[0]}}),
-  //   .res(B)
-  // );
-
-  ripple_carry #(.D_SIZE(D_SIZE)) rc0 (
-    .a(w1),
-    .b(tempP),
-    .sum(tempP)  
-  );
-
-  always_ff @(posedge clk_in) begin
-    // If we are currently in the multyply state
-    if (mult == 1) begin
-      count = count + 1;
-      if (count == D_SIZE - 1 )
-        tempA
-      else 
-        if (temp) 
-        tempA = tempA >> 1;
-        
-      mult = 0;
-    end
-  end
-
-  always_ff @(A, B) begin
-    @(negedge clk_in)
-    tempA = A;
-    tempB = B;
-    tempP = 0;
-    mult == 1;
-    
-  
-    if (tempA[0] == 1) begin
-      
-    end 
-      
-      // 
-
-
-    end
 /*
-    init P gets 0
+  Functionality: 4 bit multiplier.
+  Description: 4 bit multiplier that uses a finite state machine internally.
+*/
 
-    if (A[0] == 1)
-    add (P, B, P)
-    else if (A[0] == 0)
-    add (P B, 0...0)
 
-    right_shift(A by one)
-    
-    repeat P times 
-  */
+module mult_controler (
+  input logic m,
+  input logic adx,
+  input logic clk_in,
+  input logic rst_in,
+  
+  output wire sh,
+  output wire add,
+  output logic done
+);
+
+typedef enum logic[4:0] {
+  S0,
+  S1,
+  S2,
+  S3,
+  S4
+} states_t;
+
+logic [3:0] cnt;
+initial assign cnt = 4;
+
+
+states_t current_state;
+states_t next_state;
+
+always_ff @(posedge clk_in or negedge rst_in) begin
+  if (!rst_in) begin
+    done <= '0;
+    current_state <= S0;
   end
+  else begin
+    current_state <= next_state;
+    $display ("current_state=", current_state);
+    $display ("next_state=", next_state);
+  end
+  
+end
 
-endmodule
+always_comb begin: next_state_logic
+  case (current_state)
+    S0: begin
+      if (adx == 0) next_state = S0;
+      else if (adx == '1) next_state = S1;
+    end
+    S1:
+      next_state = S2;
+    S2:
+      next_state = S3;
+    S3:
+      next_state = S4;
+    S4:
+      next_state = S1;
+  endcase
+end
+
+always_comb begin: output_logic
+    if (current_state == S4) done = '1;
+
+end
+// always_ff @(posedge clk_in) begin
+//   if (cnt <= 0) done = 1;
+//   cnt <= cnt - 1;
+
+  
+
+
+
+
+
+endmodule: mult_controler
+
+// module multiplier (
+//   input wire[3:0] x,
+//   input wire[3:0] y,
+//   input wire start,
+//   input wire clk_in,
+//   input wire rst_in,
+//   output logic[3:0] product,
+//   output logic ready
+// );
+
+//   typedef enum logic[1:0] {
+//     IDLE,
+//     RUNNING,
+//     FINISHING
+//   } states_t;
+
+//   states_t current_state;
+//   states_t next_state;
+
+//   logic [3:0] tmpx;
+//   logic [3:0] tmpy;
+//   logic [3:0] tmpProduct;
+//   logic done;
+//   initial assign current_state = IDLE;
+
+//   always_ff @(negedge rst_in) begin
+//     tmpx <= 0;
+//     tmpy <= 0;
+//     current_state <= IDLE;
+//   end
+
+//   always @(posedge clk_in) begin
+//     current_state <= next_state;
+//   end;
+
+//   always_comb begin: next_state_logic
+//     case (current_state) 
+//       IDLE: 
+//         if (start) next_state = RUNNING;
+//         else next_state = IDLE;
+//       RUNNING:
+//         if (done) next_state = FINISHING;
+//         else next_state = RUNNING;
+//       FINISHING:
+//         next_state = IDLE;
+//     endcase
+//   end;
+
+//   always_comb begin: output_logic 
+//     case (current_state)
+//       IDLE:
+//         if (start) begin: load_registers
+//           tmpx = x;
+//           tmpy = y;
+//         end   
+//       RUNNING:begin
+//         // m = tmpy[0];
+        
+//         $display ("Running. tmpy[0] = %b", tmpy[0]);
+//         tmpy = tmpy >> 1;
+//       end
+//       FINISHING: begin
+//         product = tmpProduct;        
+//         ready = 1;
+//       end
+//         //Do nothing
+      
+//     endcase 
+//   end;
+//   logic m;
+//   logic adx;
+//   wire sh;
+//   wire add;
+  
+//   mult_controler mu0 (
+//     .m(m),
+//     .adx(adx),
+//     .sh(sh),
+//     .add(add),
+//     .done(done)
+//   );
+  
+//   // salways_ff @()
+
+// endmodule 
--- multiplier/multiplier_tb.sv
+++ multiplier/multiplier_tb.sv
@@ -2,33 +2,86 @@
   Verilog implimentation of multiplier
 */
 
-module multiplier_compare #(parameter int D_SIZE) (
-  input wire [D_SIZE-1 : 0] a,
-  input wire [D_SIZE-1 : 0] b,
-  output logic [D_SIZE * 2:0] p
-);
 
-  always_comb begin
-    p = a * b;
+module multiplier_tb ();
+// module mult_controler_tb ();
+  logic m;
+  logic adx;
+  logic clk_in;
+  logic rst_in;
+
+  logic sh;
+  logic add;
+  logic done;
+
+  initial begin
+    clk_in <= 0;
+    forever #5 clk_in = ~clk_in;
   end
 
-endmodule 
-
-module multiplier_tb #(parameter int D_SIZE = 8) ();
-  logic [D_SIZE-1 : 0] multicand;
-  logic [D_SIZE-1 : 0] multiplier;
-  logic [D_SIZE * 2:0] product;
-  
-  multiplier_compare #(.D_SIZE(D_SIZE)) u0  (
-    .a(multicand),
-    .b(multiplier),
-    .p(product)
+  mult_controler m0 (
+    .m (m),
+    .adx (adx),
+    .clk_in (clk_in),
+    .rst_in (rst_in),
+    .sh (sh),
+    .add (add),
+    .done (done)
   );
 
   initial begin
-    $display("mcand\t\tmplier\t\tprod");
-    multicand = 4'b0010; multiplier = 4'b0010;
-    #10
-    $display("%b\t\t%b\t\t%b", multicand, multiplier, product);
+    @(negedge clk_in);
+    rst_in = '0;
+    @(negedge clk_in);
+    rst_in = '1;
+    adx = '1;
+    while (!done) @(negedge clk_in);
+    $display ("Yay it works!");
+    $finish;
   end
+
 endmodule
+
+//   logic [3:0] x;
+//   logic [3:0] y;
+//   logic [3:0] product;
+  
+//   logic ready;
+//   logic start;
+//   logic rst_in;
+//   logic clk;
+
+//   initial begin
+//     clk <= 0;
+//     forever #5 clk = ~clk;
+//     //every 5 ns switch...so period of clock is 10 ns...100 MHz clock
+//   end
+
+
+//   initial begin
+    
+//     rst_in = 1;
+//     x = 0;
+//     y = 0;
+    
+//     @(negedge clk);
+//     rst_in = 0;
+//     @(negedge clk);
+//     rst_in = 1;
+//     x = $random();
+//     y = $random();
+//     start = 1;
+//     #10
+//     @(negedge clk);
+//     start = 0;
+//     while (!ready) @(negedge clk);
+//     $display ("product_b=%b", product);
+//     $display ("product_d=%d", product);
+  
+  
+//     // $display("mcand\t\tmplier\t\tprod");
+//     // multicand = 4'b0010; multiplier = 4'b0010;
+//     // #10
+//     // $display("%b\t\t%b\t\t%b", multicand, multiplier, product);
+//   end
+// endmodule
